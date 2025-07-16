6e0b4b060a30566de7c14686c5e8e74552687f53
Bmonzon <bmonzon1024@gmail.com>
2025-07-15 19:23:33 -0600

    Commit 3

--- Actividad 06.py
+++ Actividad 06.py
@@ -1,6 +1,6 @@
 def ingresar_producto(inventario):
     while True:
-        codigo = input("Ingrese el código del producto (ej. P001): ").strip()
+        codigo = input("Ingrese el código del producto: ").strip()
         if codigo in inventario:
             print("\Ese código ya existe. Intente con otro.")
         else:
@@ -34,7 +34,7 @@ def ingresar_producto(inventario):
 
     while True:
         try:
-            cantidad = int(input("Ingrese la cantidad en stock (entero positivo): "))
+            cantidad = int(input("Ingrese la cantidad en stock: "))
             if cantidad >= 0:
                 break
             else:
@@ -49,3 +49,24 @@ def ingresar_producto(inventario):
         "precio": precio,
         "cantidad": cantidad
     }
+
+def mostrar_inventario(inventario):
+        print("\n\Inventario completo:")
+        for codigo, datos in inventario.items():
+            print(f"\nCódigo: {codigo}")
+            for clave, valor in datos.items():
+                print(f"  {clave.capitalize()}: {valor}")
+
+def buscar_producto(inventario):
+        codigo = input("\nIngrese el código del producto a buscar: ").strip()
+        producto = inventario.get(codigo)
+        if producto:
+            print(f"\nProducto encontrado ({codigo}):")
+            for clave, valor in producto.items():
+                print(f"  {clave.capitalize()}: {valor}")
+        else:
+            print("Producto no encontrado.")
+def calcular_valor_total(inventario):
+    total = sum(p["precio"] * p["cantidad"] for p in inventario.values())
+    print(f"\nValor total del inventario: Q{total:.2f}")
+
